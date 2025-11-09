4b5502f75f84f4954b55bbdf7329234414a489b6
Manoj8762 <omega2d9q311@gmail.com>
2025-11-09 23:10:10 +0530

    second day

---
+++ DSA/Functional_recuursion.py
@@ -0,0 +1,13 @@
+def sum_num(n):
+    if n==1:
+        return 1
+    return n+sum_num(n-1)
+print(sum_num(5))
+    
+    
+def fact_num(n):
+    if n==1:
+        return 1
+    return n*fact_num(n-1)
+print(fact_num(5))
+    
